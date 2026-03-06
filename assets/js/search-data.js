ac45ad20d2aa436f6c45468156200ce419baec10
zhao-weiyi <zhaoweiyi21@gmail.com>
2026-03-06 08:24:06 +0000

    Deploying to gh-pages from @ zhao-weiyi/zhao-weiyi.github.io@6d85610a70a9d058f562e2816429b12f49345bdd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,6 +80,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=N5HhtJYAAAAJ&hl=en", "_blank");
         },
       },{
+        id: 'social-ssrn_id',
+        title: 'Ssrn_id',
+        section: 'Socials',
+        handler: () => {
+          window.open("", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
